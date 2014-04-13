8730d66712ea842c4f977d4d28c4be29b7441f84
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2014-04-12 22:36:56 -0400

    update test app for wrench

--- test_app/app/models/person.rb
+++ test_app/app/models/person.rb
@@ -1,3 +1,11 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
+
+  def take_a_walk to_place
+    {
+      work: "you walk to work",
+      home: "you walk home",
+      dark_alley: "you are eaten b a grue",
+    }.fetch(to_place) { fail ArgumentError }
+  end
 end
--- test_app/spec/models/person_spec.rb
+++ test_app/spec/models/person_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
-  it "fails" do
+  describe "#take_a_walk" do
+    it "walks you to work" do
+      expect(subject.take_a_walk :work).to eq("you walk to work")
+    end
+
+    it "walks you home" do
+      expect(subject.take_a_walk :home).to eq("you walk home")
+    end
+
+    it "walks you home" do
+      expect(subject.take_a_walk :dark_alley).to eq("you are eaten by a grue")
+    end
+
+    it "takes exception to other options" do
+      expect{subject.take_a_walk :not_a_place}.to raise_error(ArgumentError)
+    end
   end
 end
